e7f32b1287e4b85fd2c05bdd70edb79a138628c9
Mitan tank <141761215+Mitan11@users.noreply.github.com>
2025-12-24 15:03:52 +0530

    Refactor App class initialization to improve middleware and route setup

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ import GenAIRouter from './routers/genAIRouter.js';
 
 class App {
     constructor() {
+        // Initialize express app
         this.app = express();
         this.port = process.env.PORT || 4000;
         this.genAIRouter = new GenAIRouter();
-
+    }
+    
+    initializeMiddlewares() {
+        // Middleware to parse JSON requests
+        this.app.use(express.json());
         this.initializeMiddlewares();
         this.initializeRoutes();
-    }
-
-    initializeMiddlewares() {
-        this.app.use(express.json());
     }
 
     initializeRoutes() {
@@ -21,11 +22,12 @@ class App {
         this.app.get('/', (req, res) => {
             res.json({ message: "API is working" });
         });
-
+        // GenAI routes
         this.app.use('/genAI', this.genAIRouter.getRouter());
     }
 
     listen() {
+        // Start the server
         this.app.listen(this.port, (error) => {
             if (error) return console.log("Connection Error \n", error);
             console.log(`Your Server is running on http://localhost:${this.port}`);
